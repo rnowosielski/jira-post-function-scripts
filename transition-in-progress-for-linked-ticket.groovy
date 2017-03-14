1f9ae15bdd076942fdfab551a3e9f2ada18dc920
Rafal Nowosielski <rnowosielski@cimpress.com>
2017-03-14 23:12:26 +0100

    Small improvements not to transition the ticket if other links exists and the linked ticket is not done. Will have to refactor this when I get some time because it got a little bit messy.

--- transition-in-progress-for-linked-ticket.groovy
+++ transition-in-progress-for-linked-ticket.groovy
@@ -8,35 +8,67 @@ import com.atlassian.jira.user.ApplicationUser
 import org.apache.log4j.Category
 def Category log = Category.getInstance("com.onresolve.jira.groovy.PostFunction")
 log.setLevel(org.apache.log4j.Level.DEBUG)
-log.debug "Will try to move linked tickets to In Progress"
 
-def transitionTicketsImplementedByCurrentOne(int transitionId) {
-    def issueLinkManager = ComponentAccessor.getIssueLinkManager();
-    ApplicationUser user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
-    WorkflowTransitionUtil workflowTransitionUtil = ( WorkflowTransitionUtil ) JiraUtils.loadComponent( WorkflowTransitionUtilImpl.class );
-    issueLinkManager.getOutwardLinks(issue.id)?.each { issueLink ->
-        log.debug "Issue relations ship considered is: " + issueLink.issueLinkType.name
-        if (issueLink.issueLinkType.name == "Implement") {
-            log.debug "Linked issue is " + issueLink.getDestinationObject().getKey();
-            MutableIssue issue = (MutableIssue) issueLink.getDestinationObject()
-            workflowTransitionUtil.setIssue(issue);
-            log.debug "Issue is set. Assigning the ticket to: " + user.getKey();
-            workflowTransitionUtil.setUserkey(user.getKey());
-            log.debug "Assigned user is set"
-            workflowTransitionUtil.setAction(transitionId)
-            log.debug "Action is set"
-            workflowTransitionUtil.validate();
-            log.debug "Validationg action"
-            workflowTransitionUtil.progress();
-            log.debug "Action executed"
+String doneStatus = "Done";
+String closedStatus = "Closed";
+String inProgressStatus = "In Progress";
+List<String> finalStatuses = [doneStatus, closedStatus]
+String implementRelationship = "Implement"
+int doneActionId = 31;
+int progressActionId = 21;
+
+def transitionTicketsImplementedByCurrentOne(int transitionId, List<String> finalStatuses, String implementRelationship, Category log) {
+    log.debug "Entering transitionTicketsImplementedByCurrentOne()";
+    try {
+        def issueLinkManager = ComponentAccessor.getIssueLinkManager();
+        ApplicationUser user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
+        WorkflowTransitionUtil workflowTransitionUtil = (WorkflowTransitionUtil) JiraUtils.loadComponent(WorkflowTransitionUtilImpl.class);
+        issueLinkManager.getOutwardLinks(issue.id)?.each { issueLink ->
+            log.debug "Issue relations ship considered is: " + issueLink.issueLinkType.name
+            if (issueLink.issueLinkType.name.equalsIgnoreCase(implementRelationship)) {
+                log.debug "Linked issue is " + issueLink.getDestinationObject().getKey();
+                MutableIssue destinationIssue = (MutableIssue) issueLink.getDestinationObject()
+
+                boolean allInwardImplementLinksDone = true;
+                for (def issueLinkNested : issueLinkManager.getInwardLinks(destinationIssue.id)) {
+                    def sourceObject = issueLinkNested.getSourceObject();
+                    def statusName = sourceObject.getStatus().getName();
+                    if (sourceObject.getKey() == issue.getKey()) {
+                        continue;
+                    }
+                    if (issueLinkNested.issueLinkType.name.equalsIgnoreCase(implementRelationship) && !finalStatuses.contains(statusName)) {
+                        allInwardImplementLinksDone = false;
+                        log.debug "Linked issue " + sourceObject.getKey() + " is not done. Will not transition " + issue.getKey();
+                        break;
+                    }
+                }
+
+                if (allInwardImplementLinksDone) {
+                    workflowTransitionUtil.setIssue(destinationIssue);
+                    log.debug "Issue is set. Assigning the ticket to: " + user.getKey();
+                    workflowTransitionUtil.setUserkey(user.getKey());
+                    log.debug "Assigned user is set"
+                    workflowTransitionUtil.setAction(transitionId)
+                    log.debug "Action is set"
+                    workflowTransitionUtil.validate();
+                    log.debug "Validationg action"
+                    workflowTransitionUtil.progress();
+                    log.debug "Action executed"
+                }
+            }
         }
+    } catch (Exception e) {
+        log.debug "Exception: " + e.getMessage();
     }
+    log.debug "Returning from transitionTicketsImplementedByCurrentOne()";
 }
 
 def statusName = issue.getStatus().getName()
 log.debug "Current ticket status name: " + statusName;
-if (statusName == "Done" || statusName == "Closed") {
-    transitionTicketsImplementedByCurrentOne(31)
-} else if (statusName == "In Progress") {
-    transitionTicketsImplementedByCurrentOne(21)
+if (statusName.equalsIgnoreCase(doneStatus) || statusName.equalsIgnoreCase(closedStatus)) {
+    log.debug "Will try to move implemented ticket to " + doneStatus;
+    transitionTicketsImplementedByCurrentOne(doneActionId, finalStatuses, implementRelationship, log)
+} else if (statusName.equalsIgnoreCase(inProgressStatus)) {
+    log.debug "Will try to move implemented ticket to " + inProgressStatus;
+    transitionTicketsImplementedByCurrentOne(progressActionId, finalStatuses, implementRelationship, log)
 }
